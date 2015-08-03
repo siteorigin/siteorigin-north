bc21745df0122cf80327b804051d3f9a885093f0
gregpriday <greg@siteorigin.com>
2015-08-04 01:36:56 +0200

    Fixed all sidebars and sidebar styling.

--- functions.php
+++ functions.php
@@ -110,8 +110,8 @@ function burst_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 
 	register_sidebar( array(
@@ -120,8 +120,8 @@ function burst_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'burst_widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -5,11 +5,11 @@
  * @package burst
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if ( ! is_active_sidebar( 'main-sidebar' ) ) {
 	return;
 }
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php dynamic_sidebar( 'main-sidebar' ); ?>
 </div><!-- #secondary -->
