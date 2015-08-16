6c217a737b69e42eaee89dbd30a278a584e21311
gregpriday <greg@siteorigin.com>
2015-08-16 23:41:04 +0200

    Changes in preparation for settings parsing

---
+++ inc/settings.conf.php
@@ -0,0 +1,13 @@
+<?php
+
+// An array mapping SCSS variable to a SiteOrigin Settings variable name
+return array(
+	'variables' => array(
+		'font__main' => 'fonts_main',
+		'font__headings' => 'fonts_heading',
+		'font__blockquote' => 'fonts_details',
+	),
+	'stylesheets' => array(
+		'style', 'woocommerce'
+	),
+);
