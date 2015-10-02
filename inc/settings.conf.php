a6a12da2177e7ce65d23f6189e036d97043c680a
Greg Priday <greg@siteorigin.com>
2015-10-02 13:46:56 +0200

    Added settings to configuration

--- inc/settings.conf.php
+++ inc/settings.conf.php
@@ -17,6 +17,15 @@ return array(
 		'color__text_medium' => 'fonts_text_medium',
 		'color__text_light' => 'fonts_text_light',
 		'color__text_meta' => 'fonts_text_meta',
+
+		// The header customizations
+		'masthead__background_color' => 'masthead_background_color',
+		'masthead__top_background_color' => 'masthead_top_background_color',
+		'masthead__border_color' => 'masthead_border_color',
+		'masthead__border_width' => 'masthead_border_width',
+		'masthead__padding' => 'masthead_padding',
+		'masthead__bottom_margin' => 'masthead_bottom_margin',
+
 	),
 	'free' => array(
 		// SASS variables that are available in the free version.
