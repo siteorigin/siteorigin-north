86dfaadce2aa3ffae1e74f63728567ef47d84dee
adiraomj <adizweb@outlook.com>
2015-12-08 11:38:27 +0530

    WooCommerce for disabled responsiveness

--- woocommerce/functions.php
+++ woocommerce/functions.php
@@ -28,6 +28,10 @@ function siteorigin_north_woocommerce_enqueue_styles( $styles ){
 		'media' => 'all'
 	);
 
+	if( 'is_woocommerce' && (siteorigin_setting( 'responsive_disable' ) == true) ) {
+		unset( $styles['woocommerce-smallscreen'] );
+	}
+
 	return $styles;
 }
 add_filter('woocommerce_enqueue_styles', 'siteorigin_north_woocommerce_enqueue_styles');
@@ -36,7 +40,6 @@ function siteorigin_north_woocommerce_enqueue_scripts( ){
 	if( !function_exists('is_woocommerce') ) return;
 
 	if( 'is_woocommerce' ) {
-		wp_enqueue_style( 'siteorigin-north-woocommerce', get_template_directory_uri() . '/woocommerce.css' );
 		wp_enqueue_script( 'siteorigin-north-woocommerce', get_template_directory_uri() . '/js/woocommerce.js', array( 'jquery' ), SITEORIGIN_THEME_VERSION );
 	}
 }
