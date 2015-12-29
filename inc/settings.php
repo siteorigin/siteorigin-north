ffbd4f83d308ab00ec84c2974c04ae92947e2cf8
gregpriday <greg@siteorigin.com>
2015-12-29 12:21:15 +0200

    Added all theme customizations

--- inc/settings.php
+++ inc/settings.php
@@ -63,6 +63,62 @@ function siteorigin_north_settings_init(){
 			)
 		),
 
+		'fonts' => array(
+			'title' => __('Fonts', 'siteorigin-north'),
+			'fields' => array(
+
+				// The font families used
+
+				'main' => array(
+					'type' => 'font',
+					'label' => __('Main Font', 'siteorigin-north'),
+					'description' => __('Main font used on your site.', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'headings' => array(
+					'type' => 'font',
+					'label' => __('Headings font', 'siteorigin-north'),
+					'description' => __('Font used for headings.', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'details' => array(
+					'type' => 'font',
+					'label' => __('Details font', 'siteorigin-north'),
+					'description' => __('Font used for smaller details.', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				// The colors
+
+				'text_dark' => array(
+					'type' => 'color',
+					'label' => __('Dark Text Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'text_medium' => array(
+					'type' => 'color',
+					'label' => __('Medium Text Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'text_light' => array(
+					'type' => 'color',
+					'label' => __('Light Text Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'text_meta' => array(
+					'type' => 'color',
+					'label' => __('Meta Text Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+			),
+		),
+
 		'structure' => array(
 			'title' => __('Page Structure', 'siteorigin-north'),
 			'fields' => array(
@@ -86,11 +142,51 @@ function siteorigin_north_settings_init(){
 						'centered' => __('Centered', 'siteorigin-north'),
 					)
 				),
+
 				'text_above' => array(
 					'type' => 'text',
 					'label' => __('Text Above', 'siteorigin-north'),
 					'description' => __('Text that goes above the main header.', 'siteorigin-north'),
-				)
+				),
+
+				'background_color' => array(
+					'type' => 'color',
+					'label' => __('Background Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'top_background_color' => array(
+					'type' => 'color',
+					'label' => __('Background Top Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'border_color' => array(
+					'type' => 'color',
+					'label' => __('Border Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'border_width' => array(
+					'type' => 'text',
+					'label' => __('Border Width', 'siteorigin-north'),
+					'sanitize_callback' => array( 'SiteOrigin_Settings_Value_Sanitize', 'measurement' ),
+					'live' => true,
+				),
+
+				'padding' => array(
+					'type' => 'text',
+					'label' => __('Padding', 'siteorigin-north'),
+					'sanitize_callback' => array( 'SiteOrigin_Settings_Value_Sanitize', 'measurement' ),
+					'live' => true,
+				),
+
+				'bottom_margin' => array(
+					'type' => 'text',
+					'label' => __('Bottom Margin', 'siteorigin-north'),
+					'sanitize_callback' => array( 'SiteOrigin_Settings_Value_Sanitize', 'measurement' ),
+					'live' => true,
+				),
 			)
 		),
 
@@ -182,18 +278,61 @@ function siteorigin_north_settings_init(){
 		'footer' => array(
 			'title' => __('Footer', 'siteorigin-north'),
 			'fields' => array(
+
 				'text' => array(
 					'type' => 'text',
 					'label' => __('Footer Text', 'siteorigin-north'),
 					'description' => __("{sitename} and {year} are your site's name and current year", 'siteorigin-north'),
 					'sanitize_callback' => 'wp_kses_post',
 				),
+
 				'constrained' => array(
 					'type' => 'checkbox',
 					'label' => __('Constrain', 'siteorigin-north'),
 					'description' => __("Constrain the footer width", 'siteorigin-north'),
-				)
-			)
+				),
+
+				'background_color' => array(
+					'type' => 'color',
+					'label' => __('Background Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'border_color' => array(
+					'type' => 'color',
+					'label' => __('Border Color', 'siteorigin-north'),
+					'live' => true,
+				),
+
+				'border_width' => array(
+					'type' => 'text',
+					'label' => __('Border Width', 'siteorigin-north'),
+					'sanitize_callback' => array( 'SiteOrigin_Settings_Value_Sanitize', 'measurement' ),
+					'live' => true,
+				),
+
+				'top_padding' => array(
+					'type' => 'text',
+					'label' => __('Top Padding', 'siteorigin-north'),
+					'sanitize_callback' => array( 'SiteOrigin_Settings_Value_Sanitize', 'measurement' ),
+					'live' => true,
+				),
+
+				'side_padding' => array(
+					'type' => 'text',
+					'label' => __('Side Padding', 'siteorigin-north'),
+					'sanitize_callback' => array( 'SiteOrigin_Settings_Value_Sanitize', 'measurement' ),
+					'live' => true,
+				),
+
+				'top_margin' => array(
+					'type' => 'text',
+					'label' => __('Top Margin', 'siteorigin-north'),
+					'sanitize_callback' => array( 'SiteOrigin_Settings_Value_Sanitize', 'measurement' ),
+					'live' => true,
+				),
+
+			),
 		)
 	) ) );
 
@@ -210,6 +349,14 @@ add_action('siteorigin_settings_init', 'siteorigin_north_settings_init');
 function siteorigin_north_settings_custom_css($css){
 	// Custom CSS Code
 	$css .= '/* style */' . "\n" .
+		'body,button,input,select,textarea {' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'.font( ${fonts_main} );' . "\n" .
+		'}' . "\n" .
+		'h1,h2,h3,h4,h5,h6 {' . "\n" .
+		'.font( ${fonts_headings} );' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'}' . "\n" .
 		'blockquote {' . "\n" .
 		'color: ${branding_accent};' . "\n" .
 		'}' . "\n" .
@@ -218,6 +365,9 @@ function siteorigin_north_settings_custom_css($css){
 		'}' . "\n" .
 		'a:hover,a:focus {' . "\n" .
 		'color: ${branding_accent_dark};' . "\n" .
+		'}' . "\n" .
+		'#header-search {' . "\n" .
+		'background: ${masthead_background_color};' . "\n" .
 		'}' . "\n" .
 		'.content-area {' . "\n" .
 		'margin: 0 -${structure_sidebar_width} 0 0;' . "\n" .
@@ -228,23 +378,120 @@ function siteorigin_north_settings_custom_css($css){
 		'.site-content .widget-area {' . "\n" .
 		'width: ${structure_sidebar_width};' . "\n" .
 		'}' . "\n" .
-		'.breadcrumbs a:hover {' . "\n" .
-		'color: ${branding_accent_dark};' . "\n" .
+		'#masthead {' . "\n" .
+		'background: ${masthead_background_color};' . "\n" .
+		'border-bottom: ${masthead_border_width} solid ${masthead_border_color};' . "\n" .
+		'padding: ${masthead_padding} 0;' . "\n" .
+		'margin-bottom: ${masthead_bottom_margin};' . "\n" .
+		'}' . "\n" .
+		'#masthead.layout-centered .site-branding {' . "\n" .
+		'margin: 0 auto ${masthead_padding} auto;' . "\n" .
+		'}' . "\n" .
+		'.masthead-sentinel {' . "\n" .
+		'margin-bottom: ${masthead_bottom_margin};' . "\n" .
+		'}' . "\n" .
+		'#topbar {' . "\n" .
+		'background: ${masthead_top_background_color};' . "\n" .
+		'border-bottom: ${masthead_border_width} solid ${masthead_border_color};' . "\n" .
+		'}' . "\n" .
+		'#topbar p {' . "\n" .
+		'color: ${fonts_text_light};' . "\n" .
+		'}' . "\n" .
+		'#secondary {' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'}' . "\n" .
+		'#secondary .widget-title {' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'}' . "\n" .
+		'#colophon {' . "\n" .
+		'margin-top: ${footer_top_margin};' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'border-top: ${footer_border_width} solid ${footer_border_color};' . "\n" .
+		'background: ${footer_background_color};' . "\n" .
+		'}' . "\n" .
+		'#colophon .widgets .widget-wrapper {' . "\n" .
+		'border-right: ${footer_border_width} solid ${footer_border_color};' . "\n" .
+		'}' . "\n" .
+		'#colophon .widgets aside {' . "\n" .
+		'padding: ${footer_top_padding} ${footer_side_padding};' . "\n" .
+		'}' . "\n" .
+		'#colophon .widgets .widget-title {' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'}' . "\n" .
+		'@media (max-width: 640px) {' . "\n" .
+		'body.responsive #colophon .widgets .widget-wrapper {' . "\n" .
+		'border-bottom: ${footer_border_width} solid ${footer_border_color};' . "\n" .
+		'}' . "\n" .
+		'}' . "\n" .
+		'#colophon .site-info {' . "\n" .
+		'border-top: ${footer_border_width} solid ${footer_border_color};' . "\n" .
+		'}' . "\n" .
+		'.entry-meta li,.entry-meta a,.entry-meta .meta-icon {' . "\n" .
+		'color: ${fonts_text_meta};' . "\n" .
+		'}' . "\n" .
 		'.entry-meta li.hovering,.entry-meta li.hovering a,.entry-meta li.hovering .meta-icon {' . "\n" .
+		'color: ${branding_accent_dark};' . "\n" .
+		'}' . "\n" .
+		'.breadcrumbs {' . "\n" .
+		'color: ${fonts_text_light};' . "\n" .
+		'}' . "\n" .
+		'.breadcrumbs a:hover {' . "\n" .
 		'color: ${branding_accent_dark};' . "\n" .
 		'}' . "\n" .
 		'.tags-list a:hover {' . "\n" .
 		'background: ${branding_accent_dark};' . "\n" .
 		'}' . "\n" .
+		'.more-link {' . "\n" .
+		'border: 1px solid ${fonts_text_dark};' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'.font( ${fonts_headings} );' . "\n" .
+		'}' . "\n" .
+		'.more-link:visited {' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'}' . "\n" .
 		'.more-link:hover {' . "\n" .
 		'background: ${branding_accent};' . "\n" .
 		'border-color: ${branding_accent};' . "\n" .
 		'}' . "\n" .
+		'.post-pagination a {' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'}' . "\n" .
 		'.post-pagination a:hover {' . "\n" .
 		'color: ${branding_accent_dark};' . "\n" .
 		'}' . "\n" .
+		'.post-pagination .current {' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'}' . "\n" .
+		'.post-pagination .page-numbers {' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'}' . "\n" .
+		'.post-pagination .prev,.post-pagination .next {' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'}' . "\n" .
+		'.comment-list li {' . "\n" .
+		'color: ${fonts_text_light};' . "\n" .
+		'}' . "\n" .
 		'.comment-list li .comment-reply-link:hover {' . "\n" .
 		'background: ${branding_accent_dark};' . "\n" .
+		'}' . "\n" .
+		'.comment-list li .info {' . "\n" .
+		'color: ${fonts_text_meta};' . "\n" .
+		'}' . "\n" .
+		'.comment-list li .author {' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'}' . "\n" .
+		'#commentform .comment-form-author input,#commentform .comment-form-email input,#commentform .comment-form-url input {' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'}' . "\n" .
+		'#commentform .comment-form-comment textarea {' . "\n" .
+		'color: ${fonts_text_medium};' . "\n" .
+		'}' . "\n" .
+		'#commentform .form-allowed-tags,#commentform .comment-notes,#commentform .logged-in-as {' . "\n" .
+		'color: ${fonts_text_meta};' . "\n" .
+		'}' . "\n" .
+		'#commentform .form-submit input {' . "\n" .
+		'color: ${fonts_text_dark};' . "\n" .
+		'.font( ${fonts_headings} );' . "\n" .
 		'}' . "\n" .
 		'#commentform .form-submit input:hover {' . "\n" .
 		'background: ${branding_accent_dark};' . "\n" .
@@ -253,6 +500,9 @@ function siteorigin_north_settings_custom_css($css){
 		'/* woocommerce */' . "\n" .
 		'.woocommerce #main ul.products li.product .price {' . "\n" .
 		'color: ${branding_accent};' . "\n" .
+		'}' . "\n" .
+		'.woocommerce button.button.alt,.woocommerce #review_form #respond .form-submit input,.woocommerce .woocommerce-message .button,.woocommerce .products .button {' . "\n" .
+		'.font( ${fonts_headings} );' . "\n" .
 		'}' . "\n" .
 		'.woocommerce button.button.alt:hover,.woocommerce #review_form #respond .form-submit input:hover,.woocommerce .woocommerce-message .button:hover,.woocommerce .products .button:hover {' . "\n" .
 		'background: ${branding_accent_dark};' . "\n" .
@@ -322,17 +572,33 @@ add_action( 'wp_head', 'siteorigin_north_menu_breakpoint_css' );
  * @return mixed
  */
 function siteorigin_north_settings_defaults( $defaults ){
+	// Branding defaults
 	$defaults['branding_logo'] = false;
+	$defaults['branding_logo_retina'] = false;
 	$defaults['branding_site_description'] = true;
 	$defaults['branding_accent'] = '#c75d5d';
 	$defaults['branding_accent_dark'] = '#a94346';
 
-	// Douuble % because values are passed through get_theme_mod so must be escaped for sprintf
+	// Font defaults
+	$defaults['fonts_text_dark'] = '#292929';
+	$defaults['fonts_text_medium'] = '#595959';
+	$defaults['fonts_text_light'] = '#898989';
+	$defaults['fonts_text_meta'] = '#b0b0b0';
+
+	// Double % because values are passed through get_theme_mod so must be escaped for sprintf
 	$defaults['structure_sidebar_width'] = '35%%';
 
+	// The masthead defaults
 	$defaults['masthead_text_layout'] = 'default';
 	$defaults['masthead_text_above'] = '';
+	$defaults['masthead_background_color'] = '#fafafa';
+	$defaults['masthead_top_background_color'] = false;
+	$defaults['masthead_border_color'] = '#d4d4d4';
+	$defaults['masthead_border_width'] = '1px';
+	$defaults['masthead_padding'] = '30px';
+	$defaults['masthead_bottom_margin'] = '30px';
 
+	// Navigation settings
 	$defaults['navigation_search'] = true;
 	$defaults['navigation_sticky'] = true;
 	$defaults['navigation_sticky_scale'] = true;
@@ -340,18 +606,29 @@ function siteorigin_north_settings_defaults( $defaults ){
 	$defaults['navigation_post'] = true;
 	$defaults['navigation_scroll_to_top'] = true;
 
+	// Responsive settings
+	$defaults['responsive_disabled'] = false;
 	$defaults['responsive_fitvids'] = true;
 	$defaults['responsive_menu_breakpoint'] = '600';
 	$defaults['responsive_menu_text'] = __('Menu', 'siteorigin-north');
 
+	// Blog settings
 	$defaults['blog_featured_archive'] = true;
 	$defaults['blog_featured_single'] = true;
 	$defaults['blog_display_date'] = true;
 	$defaults['blog_display_author'] = true;
 	$defaults['blog_display_comment_count'] = true;
 
+	// Footer defaults
 	$defaults['footer_text'] = __('Copyright © {year} {sitename}', 'siteorigin-north');
 	$defaults['footer_constrained'] = false;
+	$defaults['footer_background_color'] = '#fafafa';
+	$defaults['footer_border_color'] = '#d4d4d4';
+	$defaults['footer_border_width'] = '1px';
+	$defaults['footer_top_padding'] = '40px';
+	$defaults['footer_side_padding'] = '40px';
+	$defaults['footer_top_margin'] = '30px';
+
 	return $defaults;
 }
 add_filter('siteorigin_settings_defaults', 'siteorigin_north_settings_defaults');
