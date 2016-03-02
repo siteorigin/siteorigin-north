77a4b32e7ac4f383639f03f4a6b21e67c850cec5
gregpriday <greg@siteorigin.com>
2016-03-02 12:16:46 +0200

    Fix for checkout button

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -142,7 +142,9 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 						<button class="button-continue-shopping"  href="<?php echo wc_get_page_permalink( 'shop' ); ?>"><?php esc_attr_e( 'Continue Shopping', 'siteorigin-north' ) ?></button>
 						<button type="submit" class="button" name="update_cart"><?php esc_attr_e( 'Update Cart', 'siteorigin-north' ); ?></button>
-						<button type="submit" class="checkout-button" name="proceed"><span class="north-icon-cart"></span> <?php esc_attr_e( 'Checkout', 'siteorigin-north' ); ?></button>
+
+						<!-- <button type="submit" class="checkout-button" name="proceed"><span class="north-icon-cart"></span> <?php esc_attr_e( 'Checkout', 'siteorigin-north' ); ?></button> -->
+						<input type="submit" class="checkout-button" name="proceed" value="<?php echo esc_attr_e( 'Checkout', 'siteorigin-north') ?>" />
 
 						<?php do_action( 'woocommerce_cart_actions' ); ?>
 
