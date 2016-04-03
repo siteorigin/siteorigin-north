818915b9510e9739f05184b5fff344838181ba55
adiraomj <adiraomj@outlook.com>
2016-04-04 00:30:44 +0530

    Changed rating hook for quick view content

--- js/woocommerce.js
+++ js/woocommerce.js
@@ -48,26 +48,29 @@ jQuery( function($){
 	$('.product-quick-view-button').click( function(e) {
 		e.preventDefault();
 
+		var $container = '#quick-view-container';
+		var $content = '#product-quick-view';
+
 		var id = $(this).attr('data-product-id');
 
 		$.post(
 			so_ajax.ajaxurl,
 			{ action: 'so_product_quick_view', product_id: id },
 			function( data ) {
-				$(document).find('#quick-view-container').find('#product-quick-view').html(data);
+				$(document).find($container).find($content).html(data);
 			}
 		);
 
-		if($(document).find('#quick-view-container').is(':hidden')) {
-			$(document).find('#quick-view-container').find('#product-quick-view').empty();
+		if($(document).find($container).is(':hidden')) {
+			$(document).find($container).find($content).empty();
 		}
 
-		$(document).find('#quick-view-container').fadeIn(300);
+		$(document).find($container).fadeIn(300);
 
 		$(window).mouseup(function (e) {
-		    var container = $("#product-quick-view");
+		    var container = $($content);
 		    if ( ! container.is(e.target) && container.has(e.target).length === 0 ) {
-		        $('#quick-view-container').fadeOut(300);
+		        $($container).fadeOut(300);
 		    }
 		});
 
