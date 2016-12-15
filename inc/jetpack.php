3646f4434b84142da2ad06effdb19d114a8c7316
adiraomj <adiraomj@outlook.com>
2016-12-15 21:30:03 +0530

    Added infinite scroll styling and template for woocommerce

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -26,9 +26,18 @@ if ( ! function_exists( 'siteorigin_north_infinite_scroll_render' ) ) :
  * Custom render function for Infinite Scroll.
  */
 function siteorigin_north_infinite_scroll_render() {
-	while ( have_posts() ) {
-		the_post();
-		get_template_part( 'template-parts/content', get_post_format() );
+	if ( function_exists( 'is_woocommerce' ) && ( is_shop() || is_woocommerce() ) ) {
+		echo '<ul class="products">';
+		while ( have_posts() ) {
+			the_post();
+			wc_get_template_part( 'content', 'product' );
+		}
+		echo '</ul>';
+	} else {
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'template-parts/content', get_post_format() );
+		}
 	}
 } // end function siteorigin_north_infinite_scroll_render
 endif;
