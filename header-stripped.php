a98007f80ea400fd4025ebc49d2f06f99ff0ec12
Andrew Misplon <amisplon@gmail.com>
2016-12-22 16:50:30 +0200

    Add a back link to stripped template

--- header-stripped.php
+++ header-stripped.php
@@ -29,18 +29,24 @@
 		</div>
 	<?php endif; ?>
 
-	<header id="masthead" class="site-header layout-<?php echo sanitize_html_class( str_replace('_', '-', siteorigin_setting( 'masthead_layout' ) ) ) ?> <?php if( siteorigin_setting('navigation_sticky') ) echo 'sticky-menu'; ?>" role="banner"
-		<?php if( siteorigin_setting( 'navigation_sticky_scale' ) ) echo 'data-scale-logo="true"' ?> >
+	<header id="masthead" class="site-header layout-<?php echo sanitize_html_class( str_replace('_', '-', siteorigin_setting( 'masthead_layout' ) ) ) ?> <?php if ( siteorigin_setting( 'navigation_sticky' ) ) echo 'sticky-menu'; ?>" role="banner"
+		<?php if ( siteorigin_setting( 'navigation_sticky_scale' ) ) echo 'data-scale-logo="true"' ?> >
 		<div class="container">
 
 			<div class="container-inner">
+
 				<div class="site-branding">
 					<?php siteorigin_north_display_logo() ?>
-					<?php if( siteorigin_setting('branding_site_description') ) : ?>
+					<?php if ( siteorigin_setting( 'branding_site_description' ) ) : ?>
 						<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 					<?php endif ?>
 				</div><!-- .site-branding -->
-			</div>
+
+				<nav id="site-navigation" class="main-navigation stripped" role="navigation">
+					<a href="" onclick="window.history.go( -1 ); return false;">Go back</a>
+				</nav><!-- #site-navigation -->
+
+			</div><!-- .container-inner -->
 
 		</div>
 
