71982edb92bdd34519dbc31e4cae22e1ebbedcb3
adiraomj <adiraomj@outlook.com>
2017-02-02 16:18:32 +0530

    Enqueued flex slider on gallery format posts

---
+++ template-parts/content-gallery.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying single gallery format posts.
+ *
+ * @package siteorigin-north
+ * @license GPL 2.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
+
+	<?php if( has_post_thumbnail() && siteorigin_setting( 'blog_featured_single' ) ) : ?>
+		<div class="entry-thumbnail">
+			<?php siteorigin_north_entry_thumbnail() ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if( siteorigin_page_setting( 'page_title' ) ) : ?>
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<?php endif; ?>
+
+	<?php siteorigin_north_breadcrumbs(); ?>
+
+	<?php if( siteorigin_page_setting( 'page_title' ) ) : ?>
+			<div class="entry-meta">
+				<?php siteorigin_north_post_meta(); ?>
+			</div><!-- .entry-meta -->
+		</header><!-- .entry-header -->
+	<?php endif; ?>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'siteorigin-north' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php siteorigin_north_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
