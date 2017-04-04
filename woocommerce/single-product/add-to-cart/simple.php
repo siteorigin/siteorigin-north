d87b520844b81130b7fcc5d91c9867ca1def7d91
adiraomj <adiraomj@outlook.com>
2017-04-04 22:44:18 +0530

    Updated simple add to cart

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -12,7 +12,7 @@
  * @see 	    http://docs.woothemes.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.1.0
+ * @version     3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -25,14 +25,7 @@ if ( ! $product->is_purchasable() ) {
 	return;
 }
 
-?>
-
-<?php
-	// Availability
-	$availability      = $product->get_availability();
-	$availability_html = empty( $availability['availability'] ) ? '' : '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>';
-
-	echo apply_filters( 'woocommerce_stock_html', $availability_html, $availability['availability'], $product );
+echo wc_get_stock_html( $product );
 ?>
 
 <?php if ( $product->is_in_stock() ) : ?>
@@ -42,19 +35,19 @@ if ( ! $product->is_purchasable() ) {
 	<form class="cart" method="post" enctype='multipart/form-data'>
 	 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
+		<?php do_action( 'woocommerce_before_add_to_cart_quantity' ); ?>
+
 	 	<?php
-	 		if ( ! $product->is_sold_individually() ) {
-	 			woocommerce_quantity_input( array(
-	 				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', 1, $product ),
-	 				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product ),
-	 				'input_value' => ( isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 )
-	 			) );
-	 		}
+		woocommerce_quantity_input( array(
+			'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+			'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+			'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : $product->get_min_purchase_quantity(),
+		) );
 	 	?>
 
-	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
+		<?php do_action( 'woocommerce_after_add_to_cart_quantity' ); ?>
 
-	 	<button type="submit" class="single_add_to_cart_button button alt"><span class="north-icon-cart" aria-hidden="true"></span> <?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><span class="north-icon-cart" aria-hidden="true"></span> <?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
