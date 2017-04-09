9dc2d06984466f68d0af72cfe31bfbcbf1d72eb8
Andrew Misplon <amisplon@gmail.com>
2017-04-09 15:28:03 +0200

    Fix WooCommerce taglist function

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -22,8 +22,8 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 	<?php do_action( 'woocommerce_product_meta_start' ); ?>
 
-	<?php if ( function_exists( 'wc_get_product_category_list' ) ) : ?>
-		<?php echo wc_get_product_category_list( $product->get_id(), ', ', '<div class="cateogry">', '</div>' ); ?>
+	<?php if ( function_exists( 'wc_get_product_tag_list' ) ) : ?>
+	    <?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<div class="tags-list">', '</div>' ); ?>
 	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
