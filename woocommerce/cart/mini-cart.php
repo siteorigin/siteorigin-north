fe94d487fa5d42b9f32e857db288b5c0ab050a42
adiraomj <adiraomj@outlook.com>
2017-10-16 17:26:35 +0530

    Updated mini-cart with ajaxify feature

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -6,7 +6,7 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.1.0
+ * @version     3.2.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -46,7 +46,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 						</div>
 						<?php
 						echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
-							'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+							'<a href="%s" class="remove remove_from_cart_button" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s">&times;</a>',
 							esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
 							__( 'Remove this item', 'siteorigin-north' ),
 							esc_attr( $product_id ),
