9e509a6700ad51781f2a00838b31e86fe9bcecd8
Andrew Misplon <amisplon@gmail.com>
2017-10-30 14:07:45 +0200

    Correct text domain

--- comments.php
+++ comments.php
@@ -62,7 +62,7 @@ if ( post_password_required() ) {
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'siteorigin-corp' ); ?></p>
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'siteorigin-north' ); ?></p>
 	<?php
 	endif;
 
