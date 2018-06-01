56b9d323506d90dd35185edc6401d8336003f455
adiraomj <adiraomj@outlook.com>
2018-06-01 13:53:42 +0530

    updated wc cart template

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -5,7 +5,7 @@
  * @see 	    http://docs.woothemes.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.0.0
+ * @version     3.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
