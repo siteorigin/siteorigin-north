8e7282726979740537a0000eebba73d47ba69eb4
Andrew Misplon <amisplon@gmail.com>
2019-09-25 14:01:59 +0200

    PHP formatting for WP

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -2,9 +2,9 @@
 /**
  * Single Product Meta
  *
- * @see 	    http://docs.woothemes.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
+ * @see         http://docs.woothemes.com/document/template-structure/
+ * @author      WooThemes
+ * @package     WooCommerce/Templates
  * @version     3.0.0
  */
 
@@ -20,7 +20,7 @@ global $post, $product;
 	<?php do_action( 'woocommerce_product_meta_start' ); ?>
 
 	<?php if ( function_exists( 'wc_get_product_tag_list' ) ) : ?>
-	    <?php echo wc_get_product_tag_list( $product->get_id(), '', '<div class="tags-list">', '</div>' ); ?>
+		<?php echo wc_get_product_tag_list( $product->get_id(), '', '<div class="tags-list">', '</div>' ); ?>
 	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
