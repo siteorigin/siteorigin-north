1d6bb8b2464824b65ea5387758c989f2b120a44a
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-12-02 08:08:15 +1000

    jQuery Deprecated Shorthand changes

--- js/woocommerce.js
+++ js/woocommerce.js
@@ -30,9 +30,9 @@ jQuery( function($){
                 $( "<li></li>" )
                     .html( $o.html() )
                     .data( 'val', $o.attr( 'value' ) )
-                    .click( function() {
+                    .on( 'click', function() {
                         $$.val( $( this ).data( 'val' ) );
-                        $$.closest( 'form' ).submit();
+                        $$.closest( 'form' ).trigger( 'submit' );
                     } )
             );
 
@@ -46,18 +46,18 @@ jQuery( function($){
     } );
 
     // Open dropdown on click.
-	$( '.ordering-selector-wrapper' ).click( function() {
+	$( '.ordering-selector-wrapper' ).on( 'click', function() {
 		$(this).toggleClass( 'open-dropdown' );
 	} );
 
 	// Closing dropdown on click outside dropdown wrapper.
-	$( window ).click( function( e ) {
+	$( window ).on( 'click', function( e ) {
 		if ( !$( e.target ).closest( '.ordering-selector-wrapper.open-dropdown' ).length ) {
 			$( '.ordering-selector-wrapper.open-dropdown' ).removeClass( 'open-dropdown' );
 		}
 	})
 
-	$('.product-quick-view-button').click( function( e ) {
+	$('.product-quick-view-button').on( 'click', function( e ) {
 		e.preventDefault();
 
 		var $container = '#quick-view-container';
@@ -79,14 +79,14 @@ jQuery( function($){
 
 		$( document ).find( $container ).fadeIn( 300 );
 
-		$( window ).mouseup( function( e ) {
+		$( window ).on( 'mouseup', function( e ) {
 		    var container = $($content);
 		    if ( ( !container.is(e.target) && container.has(e.target).length === 0 ) || $( '.quickview-close-icon' ).is( e.target ) ) {
 		        $( $container ).fadeOut( 300 );
 		    }
 		} );
 
-        $( document ).keyup( function( e ) {
+        $( document ).on( 'keyup', function( e ) {
 			var container = $( $content );
 			if ( e.keyCode == 27 ) { // Escape key maps to keycode `27`
 				$( $container ).fadeOut( 300 );
